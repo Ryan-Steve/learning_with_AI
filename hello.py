dce0ee211104ca5f6627568401a080364a78c90a
Ryan-Steve <ryansotieno747@gmail.com>
2026-02-16 21:51:54 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,40 @@
+#Exercise Day 1
+print("My name is Stevieee")
+print("I am",19 * 365,"days old")
+print((100 + 50)*2 - 25)
+
+#Exercise Day 2
+Fav_movie = "Need for Speed"
+release_date = 2014
+rating = 9.5 
+print("\n My favourite movie  is",Fav_movie,"released in",release_date,"and I'd rate it",rating,"/10")
+
+
+coffee_price = 4.5
+no_of_coffee = 3
+cost = coffee_price * no_of_coffee
+print("\n Total cost of coffee is $",cost)
+
+#Exercise Day 3
+email = "stevie@gmail.com"
+print(email.upper())
+city = "Nairobi"
+country = "kenya"
+print("\n I live in",city.upper(),",",country.capitalize())
+sentence= "Naibori is the capital of Kenya"
+print(sentence.replace("Naibori","Nairobi"))
+
+#Excercise 3, Day 2
+#Commercial arithmetic
+#cash = 100
+#item = 23
+#items_bought = cash//item
+#remainder = cash%item
+
+#print("Items bought:",items_bought)
+#print("Cash remaining :",remainder)
+
+
+#DAY 3
+#Excercise 1
+
